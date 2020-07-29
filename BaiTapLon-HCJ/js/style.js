57fa56e8c731c1aff7e84acce0a05d34c3c23652
hoanghoa213 <hoangnh@dhhp.edu.vn>
2020-07-29 16:32:54 +0700

    about blog blog-detail contact product
    about blog blog-detail contact product

--- BaiTapLon-HCJ/js/style.js
+++ BaiTapLon-HCJ/js/style.js
@@ -9,4 +9,15 @@ $(function() {
 		var text = $(this).text();
 		$(".unit").text(text);
 	});
+	$( "#slider-range" ).slider({
+      range: true,
+      min: 0,
+      max: 500,
+      values: [ 75, 300 ],
+      slide: function( event, ui ) {
+        $( "#amount" ).val( "$" + ui.values[ 0 ] +".00"+ " - $" + ui.values[ 1 ]+".00" );
+      }
+    });
+    $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 )+".00" +
+      " - $" + $( "#slider-range" ).slider( "values", 1 )+".00" );
 });
